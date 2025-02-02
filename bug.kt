f14f8fe6e810c3a5e29067a3a9e446da1bdb1287
BugHunter-X <askzenai@gmail.com>
2025-02-02 20:48:06 +0100

    Add bug.kt file

---
+++ bug.kt
@@ -0,0 +1,11 @@
+```kotlin
+fun main() {
+    val list = listOf(1, 2, 3, 4, 5)
+    val result = list.map { it * 2 }
+    println(result) // Output: [2, 4, 6, 8, 10]
+
+    val mutableList = mutableListOf(1,2,3)
+    mutableList.map {it * 2}
+    println(mutableList) //Output: [1,2,3]
+}
+```
